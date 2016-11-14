82d2163de710a89be596b244d380f6df647c87ab
Guillaume Carre <guillaume.carre@gmail.com>
2016-11-14 10:48:46 -0800

    Update docker script to restart always
    The bot crashed over the weekend and docker didn't restart it automatically, which means it didn't exit with an error code.

--- jirabot.sh
+++ jirabot.sh
@@ -2,4 +2,4 @@
 
 docker pull ticketfly/jirabot
 
-docker run --restart=on-failure -d -e SLACK_API_TOKEN=$SLACK_API_TOKEN -e JIRA_USER=$JIRA_USER -e JIRA_PASS=$JIRA_PASS -e JIRA_PREFIX=$JIRA_PREFIX ticketfly/jirabot
+docker run --restart=always -d -e SLACK_API_TOKEN=$SLACK_API_TOKEN -e JIRA_USER=$JIRA_USER -e JIRA_PASS=$JIRA_PASS -e JIRA_PREFIX=$JIRA_PREFIX ticketfly/jirabot
